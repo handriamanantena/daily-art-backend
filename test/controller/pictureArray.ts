fef811f5c21c3f01138235e6f87c523b2882eda3
harrison <harrison.a101@hotmail.com>
2023-05-31 14:39:36 -0400

    mock db data for jest test

---
+++ test/controller/pictureArray.ts
@@ -0,0 +1,304 @@
+import {ObjectId} from "mongodb";
+
+export let picturesDB = [{
+  "_id": new ObjectId("613012d4d165b61ffc1896f6"),
+  "pictureName": "naruto",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": new Date("2023-04-16T03:15:19.313Z"),
+  "width": "1035",
+  "height": "1228",
+  "author": "RandomDude",
+  "recentComments": [
+    {
+      "nameOfCommenter": "Harrison",
+      "likeCount": 2,
+      "comment": "test 2 object id",
+      "_id": new ObjectId("6354a64f189d795c7a37007c")
+    },
+    {
+      "nameOfCommenter": "Harrison",
+      "likeCount": 2,
+      "comment": "test object id"
+    },
+    {
+      "nameOfCommenter": "Harrison",
+      "likeCount": 2,
+      "comment": "reply 1"
+    },
+    {
+      "nameOfCommenter": "Harrison",
+      "commenterId": 2135,
+      "likeCount": 2,
+      "comment": "comment 7"
+    },
+    {
+      "nameOfCommenter": "Harrison",
+      "commenterId": 2135,
+      "likeCount": 2,
+      "comment": "comment 6"
+    },
+    {
+      "nameOfCommenter": "Harrison",
+      "commenterId": 2135,
+      "likeCount": 2,
+      "comment": "comment 5"
+    }
+  ],
+  "replies": [
+    {
+      "nameOfCommenter": "delete",
+      "comment": "test comment",
+      "nameOfOriginalCommenter": "deez"
+    },
+    {
+      "nameOfCommenter": "delete",
+      "comment": "test comment",
+      "nameOfOriginalCommenter": "deez"
+    },
+    {
+      "nameOfCommenter": "delete",
+      "comment": "test comment",
+      "nameOfOriginalCommenter": "deez"
+    },
+    {
+      "nameOfCommenter": "Harrison",
+      "likeCount": 2,
+      "comment": "test recent comment"
+    }
+  ]
+},{
+  "_id": new ObjectId("6136f49367c9c0458ef84685"),
+  "pictureName": "naruto",
+  "url": "img - Copy.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": new Date("2023-04-16T03:15:20.320Z"),
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("6136f5ce67c9c0458ef84686"),
+  "pictureName": "naruto",
+  "url": "img - Copy (2).png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": new Date("2023-04-16T03:15:21.329Z"),
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("6136f5d667c9c0458ef84687"),
+  "pictureName": "naruto",
+  "url": "img - Copy (3).png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:22.337Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("6136f5e267c9c0458ef84688"),
+  "pictureName": "naruto",
+  "url": "img - Copy (4).png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:23.346Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("6136f75267c9c0458ef846a9"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:24.355Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663bfaab09ef5642b302a"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:25.364Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663c4aab09ef5642b302b"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": new Date("2023-04-16T03:15:26.367Z"),
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663d7aab09ef5642b302c"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date":  new Date("2023-04-16T03:15:27.376Z"),
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663dcaab09ef5642b302d"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": new Date("2023-04-16T03:15:28.383Z"),
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663e0aab09ef5642b302e"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:29.388Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663e7aab09ef5642b302f"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:30.395Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663ebaab09ef5642b3030"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:31.402Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663eeaab09ef5642b3031"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:32.409Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663f3aab09ef5642b3032"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:33.415Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663f5aab09ef5642b3033"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:34.422Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663f9aab09ef5642b3034"),
+  "pictureName": "hokage",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": {
+    "$date": "2023-04-16T03:15:35.427Z"
+  },
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("642663fdaab09ef5642b3035"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": new Date("2023-04-16T03:15:36.436Z"),
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("64266401aab09ef5642b3036"),
+  "pictureName": "duplicate",
+  "url": "img.png",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": new Date("2023-05-16T03:15:37.443Z"),
+  "width": "1035",
+  "height": "1228"
+},{
+  "_id": new ObjectId("6467d2a9f0067f9f2733bd6f"),
+  "pictureName": "Madara",
+  "url": "madara.jpg",
+  "tags": [
+    "anime",
+    "naruto"
+  ],
+  "date": new Date("2023-05-16T04:15:37.443Z"),
+  "width": "1035",
+  "height": "1228"
+}]
