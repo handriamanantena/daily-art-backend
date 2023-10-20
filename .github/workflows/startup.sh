40d5a29e178936066a4c3ec6e995ceace3e66522
harrison <harrison.a101@hotmail.com>
2023-10-19 21:07:38 -0400

    fixed typo

--- .github/workflows/startup.sh
+++ .github/workflows/startup.sh
@@ -4,6 +4,6 @@ cd ..
 pm2 stop dailyart-1
 pm2 stop dailyart-2
 pm2 start pm2Instance1.config.js
-pm2 start pm2Instance.2config.js
+pm2 start pm2Instance2.config.js
 export BLUE=3000
 export GREEN=3001
