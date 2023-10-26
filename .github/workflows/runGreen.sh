3199936bf0c3952f9e2d75b8f871908ec7efe4c3
harrison <harrison.a101@hotmail.com>
2023-10-26 15:07:48 -0400

    fixed blue green switch

--- .github/workflows/runGreen.sh
+++ .github/workflows/runGreen.sh
@@ -3,11 +3,11 @@
 # determine file and port
 CURRENT_PORT=$(grep -oP 'proxy_pass http://localhost:\K\d+' /etc/nginx/conf.d/dev-api.dailyirasuto.com.conf)
 if [ "$CURRENT_PORT" == 3001 ]; then
-    GREEN_PORT="3001"
-    BLUE_PORT="3000"
-else
     GREEN_PORT="3000"
     BLUE_PORT="3001"
+else
+    GREEN_PORT="3001"
+    BLUE_PORT="3000"
 fi
 
 docker image rm registry.digitalocean.com/dailyirasuto-backend-images/dailyart:staging || true
