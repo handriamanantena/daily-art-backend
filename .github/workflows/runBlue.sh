a710299f124120fde8daff29cc7df4c4235a694d
harrison <harrison.a101@hotmail.com>
2023-10-26 02:36:19 -0400

    hard code current port

--- .github/workflows/runBlue.sh
+++ .github/workflows/runBlue.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # determine file and port
-CURRENT_PORT=$(grep -oP 'proxy_pass http://localhost:\K\d+' /etc/nginx/conf.d/dev-api.dailyirasuto.com.conf)
+#CURRENT_PORT=$(grep -oP 'proxy_pass http://localhost:\K\d+' /etc/nginx/conf.d/dev-api.dailyirasuto.com.conf)
+CURRENT_PORT=3000
 if [ "$CURRENT_PORT" == 3001 ]; then
     GREEN_PORT="3001"
     BLUE_PORT="3000"
