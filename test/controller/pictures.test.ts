f3ea8a14a7b4838b7ed2d92083f02bd61258c4a0
harrison <harrison.a101@hotmail.com>
2023-12-30 21:10:42 -0500

    fixed unit tests

--- test/controller/pictures.test.ts
+++ test/controller/pictures.test.ts
@@ -1,14 +1,11 @@
-import {closeConnection, collections, connectToDatabase} from "../../dbConnection/dbConn";
-
-import {MongoDBClient} from "../../dbConnection/MongoDBClient";
-const mongoDBClient = new MongoDBClient();
+import {connection, client as mongoDBClient} from "../../dbConnection/MongoDBConnection";
 import {picturesDB} from "./pictureArray";
-import {Picture} from "../../model/picture";
+import {PictureDB} from "../../model/picture";
+import * as mongoDB from "mongodb";
 // @ts-ignore
 describe('test get picture', () => {
-    let connection;
-    let db;
 
+    let collection : mongoDB.Collection;
     // @ts-ignore
     beforeAll(async () => {
         // @ts-ignore
@@ -17,17 +14,17 @@ describe('test get picture', () => {
             useUnifiedTopology: true,
         });*/
         const uri = global.__MONGO_URI__;
-        await connectToDatabase(uri, {
+        await connection.connectToDatabase(uri, {
         }, globalThis.__MONGO_DB_NAME__);
 
-        const pictureCollection = collections.pictures;
+        collection = await connection.mongoDB.getCollection("pictures");
 
-        await pictureCollection?.insertMany(picturesDB);
+        await collection?.insertMany(picturesDB);
     });
 
     // @ts-ignore
     afterAll(async () => {
-        await closeConnection();
+        await connection.closeConnection();
     });
 
     test('get 1 picture', async () => {
@@ -41,25 +38,23 @@ describe('test get picture', () => {
     });
 
     test('Test get first 3 pictures', async () => {
-        let pictures : Picture[] = await mongoDBClient.getResourceByPage("pictures", "", 3, {}, "", {}) as Picture[];
+        let pictures : PictureDB[] = await mongoDBClient.getResourceByPage("pictures", "", 3, {}, "", {}) as PictureDB[];
         expect(pictures.length).toEqual( 3);
-        console.log(pictures);
         expect(pictures[0]._id?.toHexString()).toEqual("6467d2a9f0067f9f2733bd6f");
         expect(pictures[1]._id?.toHexString()).toEqual("64266401aab09ef5642b3036");
         expect(pictures[2]._id?.toHexString()).toEqual("642663fdaab09ef5642b3035");
     });
 
     test('Test get first 3 pictures with filter, but only return 2', async () => {
-        let pictures : Picture[] = await mongoDBClient.getResourceByPage("pictures", "6136f5ce67c9c0458ef84686", 3, { date: { $lt: new Date("2023-04-16T03:15:21.329Z")}}, "", {}) as Picture[];
-        let picture = await collections.pictures?.findOne({});
+        let pictures : PictureDB[] = await mongoDBClient.getResourceByPage("pictures", "6136f5ce67c9c0458ef84686", 3, { date: { $lt: new Date("2023-04-16T03:15:21.329Z")}}, "", {}) as PictureDB[];
+        let picture = await collection.findOne({});
         expect(pictures.length).toEqual( 2);
         expect(pictures[0]._id?.toHexString()).toEqual("6136f49367c9c0458ef84685");
         expect(pictures[1]._id?.toHexString()).toEqual("613012d4d165b61ffc1896f6");
     });
 
     test('Test get first 3 pictures with filter', async () => {
-        let pictures : Picture[] = await mongoDBClient.getResourceByPage("pictures", "642663e0aab09ef5642b302e", 3, { date: { $lt: new Date("2023-04-16T03:15:29.388Z")}}, "", {}) as Picture[];
-        console.log(JSON.stringify(pictures));
+        let pictures : PictureDB[] = await mongoDBClient.getResourceByPage("pictures", "642663e0aab09ef5642b302e", 3, { date: { $lt: new Date("2023-04-16T03:15:29.388Z")}}, "", {}) as PictureDB[];
         expect(pictures.length).toEqual( 3);
         expect(pictures[0]._id?.toHexString()).toEqual("642663dcaab09ef5642b302d");
         expect(pictures[1]._id?.toHexString()).toEqual("642663d7aab09ef5642b302c");
